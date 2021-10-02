d063654fda3a8c8a2b5690bb19565be1e3df81d8
lmirenda <86894184+lmirenda@users.noreply.github.com>
2021-10-02 16:19:31 -0300

    Clase 2

---
+++ src/componentes/HomeView/HomeView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const HomeView = () => {
+
+    return <h1>Hola Coder!</h1>
+
+}
+
+export default HomeView
---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
