424326e7896fd5e5ec9c80ef325f453f952df10d
lmirenda <86894184+lmirenda@users.noreply.github.com>
2021-11-18 00:53:02 -0300

    NavBar Styling

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -13,7 +13,7 @@ const brandName = "TiendaTecno"
 export const NavBar = () => {
     return (
         <>
-            {/* <Navbar bg="dark" expand="lg">
+            <Navbar bg="dark" expand="lg">
                 <Container>
                     <Navbar.Brand href="#home"> <NavLink activeClassName="activeLink" exact to="/">{brandName}</NavLink></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -23,26 +23,26 @@ export const NavBar = () => {
                         <NavDropdown.Item href="/productos/PC"><NavLink activeClassName="activeLink" to="/productos/PC">PC</NavLink></NavDropdown.Item>
                         <NavDropdown.Item href="/productos/Mac"><NavLink activeClassName="activeLink" to="/productos/Mac">Mac</NavLink></NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#link"><NavLink activeClassName="activeLink" to="/contacto">Contacto</NavLink></Nav.Link>
+                        {/* <Nav.Link href="#link"><NavLink activeClassName="activeLink" to="/contacto">Contacto</NavLink></Nav.Link> */}
                         <Nav.Link href="#link"><NavLink activeClassName="activeLink" to="/cart"><CartWidget/></NavLink></Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-             */}
+            
     
 
-        <Navbar bg="dark" variant="dark">
+        {/* <Navbar bg="dark" variant="dark">
             <Container>
             <Navbar.Brand href="/">{brandName}</Navbar.Brand>
             <Nav className="me-auto">
-            <Nav.Link href="/productos" className="activeLink">Productos</Nav.Link>
+            <Nav.Link href='' to path="/productos" className="activeLink">Productos</Nav.Link>
             <Nav.Link href="/productos/PC" className="activeLink">PC</Nav.Link>
             <Nav.Link href="/productos/Mac" className="activeLink">MAC</Nav.Link>
             </Nav>
             <Nav.Link href="/cart" className="cartWidget" ><CartWidget /></Nav.Link>
             </Container>
-        </Navbar>
+        </Navbar> */}
 
         </>
         
